25da64b5f80411e6e405af2c33f41f39f2e76ca4
jhpark0888 <jhpark0888@naver.com>
2025-04-16 15:38:17 +0900

    [feat] #6 - 이벤트 슬라이더 프로그래스바 및 인디케이터 구현

--- assets/js/event_slider.js
+++ assets/js/event_slider.js
@@ -4,23 +4,38 @@ const eventItems = document.querySelectorAll('.event-swiper .inner .swiper-conta
 const btnPrev = document.querySelector('.event-swiper .inner .swiper-container .swiper-button-prev');
 const btnNext = document.querySelector('.event-swiper .inner .swiper-container .swiper-button-next');
 
+const spanCurrentIndex = document.querySelector('.event-swiper .inner .swiper-container .swiper-pagination .swiper-pagination-current');
+const spanTotalIndex = document.querySelector('.event-swiper .inner .swiper-container .swiper-pagination .swiper-pagination-total');
+
+const progressBar = document.querySelector('.event-swiper .inner .swiper-container .swiper-progressbar .swiper-scrollbar-drag');
+
 const maxIndex = eventItems.length - 3;
 let currentIndex = 1;
+
+const bgProgressBarWidth = 856;
+const progressBarUnit = bgProgressBarWidth / eventItems.length
+const progressBarWidth = (progressBarUnit * 4).toFixed(3);
 
 function showNextEvent() {
     if (maxIndex <= currentIndex) return;
     currentIndex += 1;
 
-    swiperWrapper.style = `transform: translate3d(-${(currentIndex - 1) * 240}px, 0px, 0px); transition-duration: 0.5s`;
+    moveSwiper((currentIndex - 1) * 240);
+    moveProgressBar((currentIndex - 1) * progressBarUnit);
+
     changeHiddenBtnNextPrev()
+    changeCurIndexText();
 }
 
 function showPrevEvent() {
     if (currentIndex <= 1) return;
     currentIndex -= 1;
-
-    swiperWrapper.style = `transform: translate3d(-${(currentIndex - 1) * 240}px, 0px, 0px); transition-duration: 0.5s`;
+    
+    moveSwiper((currentIndex - 1) * 240);
+    moveProgressBar((currentIndex - 1) * progressBarUnit);
+    
     changeHiddenBtnNextPrev();
+    changeCurIndexText();
 }
 
 function changeHiddenBtnNextPrev() {
@@ -31,5 +46,24 @@ function changeHiddenBtnNextPrev() {
     else btnNext.hidden = false;
 }
 
-btnPrev.addEventListener('click', showPrevEvent);
-btnNext.addEventListener('click', showNextEvent);
+function changeCurIndexText() {
+    spanCurrentIndex.innerHTML = currentIndex;
+}
+
+function moveSwiper(x) {
+    swiperWrapper.style = `transform: translate3d(-${x}px, 0px, 0px); transition-duration: 0.5s`;
+}
+
+function moveProgressBar(x) {
+    progressBar.style = `width: ${progressBarWidth}px; transform: translate3d(${x}px, 0px, 0px); transition-duration: 0.5s;`
+}
+
+function init() {
+    btnPrev.addEventListener('click', showPrevEvent);
+    btnNext.addEventListener('click', showNextEvent);
+        
+    spanTotalIndex.innerHTML = maxIndex;
+    moveProgressBar(0)
+}
+
+init();
