8195431e514058d5176ddcf36907699d2d20ef7c
jhpark0888 <jhpark0888@naver.com>
2025-04-17 20:18:45 +0900

    [feat] #14 - 이벤트 슬라이더 슬라이드 구현

--- assets/js/event_slider.js
+++ assets/js/event_slider.js
@@ -13,34 +13,30 @@ fetch('event_slider.html')
     const spanTotalIndex = document.querySelector('.event-swiper .inner .swiper-container .swiper-pagination .swiper-pagination-total');
 
     const progressBar = document.querySelector('.event-swiper .inner .swiper-container .swiper-progressbar .swiper-scrollbar-drag');
-
+    
+    const BG_PROGRESSBAR_WIDTH = 856;
+    const PROGRESSBAR_UNIT = BG_PROGRESSBAR_WIDTH / eventItems.length
+    const PROGRESSBAR_WIDTH = (PROGRESSBAR_UNIT * 4).toFixed(3);
+    let curProgressBarOffsetX = progressBar.offsetLeft;
+    
+    const EVENT_SLIDE_WIDTH = 240;
+    let curSwiperOffsetX = swiperWrapper.offsetLeft;
     const maxIndex = eventItems.length - 3;
     let currentIndex = 1;
 
-    const bgProgressBarWidth = 856;
-    const progressBarUnit = bgProgressBarWidth / eventItems.length
-    const progressBarWidth = (progressBarUnit * 4).toFixed(3);
+    let startX = 0;
+    let isDragging = false;
 
     function showNextEvent() {
         if (maxIndex <= currentIndex) return;
-        currentIndex += 1;
 
-        moveSwiper((currentIndex - 1) * 240);
-        moveProgressBar((currentIndex - 1) * progressBarUnit);
-
-        changeHiddenBtnNextPrev()
-        changeCurIndexText();
+        moveToIndex(currentIndex + 1);
     }
 
     function showPrevEvent() {
         if (currentIndex <= 1) return;
-        currentIndex -= 1;
 
-        moveSwiper((currentIndex - 1) * 240);
-        moveProgressBar((currentIndex - 1) * progressBarUnit);
-
-        changeHiddenBtnNextPrev();
-        changeCurIndexText();
+        moveToIndex(currentIndex - 1);
     }
 
     function changeHiddenBtnNextPrev() {
@@ -55,19 +51,68 @@ fetch('event_slider.html')
         spanCurrentIndex.innerHTML = currentIndex;
     }
 
-    function moveSwiper(x) {
-        swiperWrapper.style = `transform: translate3d(-${x}px, 0px, 0px); transition-duration: 0.5s`;
+    function moveSwiper(x, duration = 0.5) {
+        swiperWrapper.style = `transform: translate3d(${x}px, 0px, 0px); transition-duration: ${duration}s`;
     }
 
-    function moveProgressBar(x) {
-        progressBar.style = `width: ${progressBarWidth}px; transform: translate3d(${x}px, 0px, 0px); transition-duration: 0.5s;`
+    function moveProgressBar(x, duration = 0.5) {
+        progressBar.style = `width: ${PROGRESSBAR_WIDTH}px; transform: translate3d(${x}px, 0px, 0px); transition-duration: ${duration}s;`
+    }
+
+    function moveToIndex(index) {
+        currentIndex = index;
+
+        curSwiperOffsetX = -(currentIndex - 1) * EVENT_SLIDE_WIDTH;
+        moveSwiper(curSwiperOffsetX);
+
+        curProgressBarOffsetX = (currentIndex - 1) * PROGRESSBAR_UNIT;
+        moveProgressBar(curProgressBarOffsetX);
+
+        changeHiddenBtnNextPrev();
+        changeCurIndexText();
     }
 
     function init() {
         btnPrev.addEventListener('click', showPrevEvent);
         btnNext.addEventListener('click', showNextEvent);
 
+        swiperWrapper.addEventListener("mousedown", (e) => {
+            isDragging = true;
+            startX = e.clientX; // 시작 지점 기록
+        });
+        
+        document.addEventListener("mousemove", (e) => {
+            if (!isDragging) return;
+
+            distance = e.clientX - startX;
+            moveSwiper(curSwiperOffsetX + distance, 0);
+
+            let progressBarDistance = distance * (PROGRESSBAR_UNIT / EVENT_SLIDE_WIDTH ) ;
+            moveProgressBar(curProgressBarOffsetX - progressBarDistance, 0);
+        });
+        
+        document.addEventListener("mouseup", (e) => {
+            if (!isDragging) return;
+            isDragging = false;
+
+            const distance = e.clientX - startX;
+            const direction = distance < 0 ? 'left' : 'right';
+            const movedCount = Math.round(Math.abs(distance) / EVENT_SLIDE_WIDTH);
+
+            let targetIdx = direction === 'left'
+                ? currentIndex + movedCount
+                : currentIndex - movedCount;
+
+            // 슬라이더 범위 제한
+            targetIdx = Math.max(1, Math.min(maxIndex, targetIdx));
+
+            console.log(distance, direction, movedCount, targetIdx);
+
+            moveToIndex(targetIdx);
+        });
+
         spanTotalIndex.innerHTML = maxIndex;
+        changeHiddenBtnNextPrev();
         moveProgressBar(0)
     }
 
