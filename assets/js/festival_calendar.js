1133527d5e0e28bc61a57981e5ac5837793ddfb7
jhpark0888 <jhpark0888@naver.com>
2025-04-17 11:33:45 +0900

    [feat] #10 - 축제 타일 UI 구현

--- assets/js/festival_calendar.js
+++ assets/js/festival_calendar.js
@@ -1,3 +1,71 @@
+const festivals = [
+    {
+        image: './assets/images/festival-calendar/festival_thumbnail1.jpg',
+        title: '피노키오 페스티벌',
+        addr: '경기 가평군',
+        detailAddr: '경기도 가평군 청평면 호반로 1063',
+        startDate: new Date(2025, 3, 7),
+        endDate: new Date(2025, 3, 21),
+    },
+    {
+        image: './assets/images/festival-calendar/festival_thumbnail2.jpg',
+        title: '대천시민천문대 별축제',
+        addr: '대전 유성구',
+        detailAddr: '대전광역시 유성구 대덕대로 480 (도룡동)',
+        startDate: new Date(2025, 3, 16),
+        endDate: new Date(2025, 3, 20),
+    },
+    {
+        image: './assets/images/festival-calendar/festival_thumbnail3.png',
+        title: '경복궁 별빛야행',
+        addr: '서울 종로구',
+        detailAddr: '서울특별시 종로구 사직로 161 (세종로)',
+        startDate: new Date(2025, 3, 2),
+        endDate: new Date(2025, 3, 16),
+    },
+    {
+        image: './assets/images/festival-calendar/festival_thumbnail4.jpg',
+        title: '용인 자작나무숲 봄꽃축제',
+        addr: '경기 용인시',
+        detailAddr: '경기도 용인시 처인구 백암면 석천리 723-2',
+        startDate: new Date(2025, 3, 19),
+        endDate: new Date(2025, 4, 1),
+    },
+    {
+        image: './assets/images/festival-calendar/festival_thumbnail5.jpg',
+        title: '서울가든페스타',
+        addr: '서울 성동구',
+        detailAddr: '서울특별시 성동구 뚝섬로 273 (성수동1가)',
+        startDate: new Date(2025, 2, 26),
+        endDate: new Date(2025, 3, 8),
+    },
+    {
+        image: './assets/images/festival-calendar/festival_thumbnail6.jpg',
+        title: '피나클랜드 봄꽃축제',
+        addr: '충남 아산시',
+        detailAddr: '충청남도 아산시 영인면 월선길 20-42',
+        startDate: new Date(2025, 2, 22),
+        endDate: new Date(2025, 3, 5),
+    },
+    {
+        image: './assets/images/festival-calendar/festival_thumbnail7.jpg',
+        title: '2025 대한민국 임시정부기념관 토크콘서트',
+        addr: '서울 서대문구',
+        detailAddr: '서울특별시 서대문구 통일로 279-24 (현저동)',
+        startDate: new Date(2025, 4, 4),
+        endDate: new Date(2025, 4, 16),
+    },
+    {
+        image: './assets/images/festival-calendar/festival_thumbnail8.jpg',
+        title: '베어트리파크 봄꽃 축제',
+        addr: '세종',
+        detailAddr: '세종특별자치시 신송로 217 베어트리파크',
+        startDate: new Date(2025, 3, 18),
+        endDate: new Date(2025, 3, 29),
+    },
+]
+
+// calendar -----------------------------------------------------
 const dows = ['일', '월', '화', '수', '목', '금', '토'];
 const calendar = document.querySelector('.festival_calendar .calendar');
 const dayTiles = calendar.querySelectorAll('.festival_calendar .calendar #dayList .mainDayList');
@@ -8,14 +76,22 @@ const now = new Date();
 const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
 let startDate = today;
 let selectedDate = today;
+// festival -----------------------------------------------------
+const ftSwiperWrapper = document.querySelector('.festival_calendar .list .swiper_container .swiper-wrapper');
+
+
+// --------------------------------------------------------------
 
 init();
 
 function init() {
     setTwoWeeks(today);
+    setFestivalTileList();
     btnPrevTwoWeeks.addEventListener('click', changeToPrevTwoWeeks);
     btnNextTwoWeeks.addEventListener('click', changeToNextTwoWeeks);
 }
+
+// calendar function ----------------------------------------------------------------
 
 function setTwoWeeks(date) {
     for (let i = 0; i < 14; i++) {
@@ -40,6 +116,7 @@ function changeToPrevTwoWeeks() {
     selectedDate = newStartDate;
     
     setTwoWeeks(startDate);
+    setFestivalTileList();
 }
 
 function changeToNextTwoWeeks() {
@@ -53,6 +130,7 @@ function changeToNextTwoWeeks() {
     startDate = newStartDate;
     selectedDate = newStartDate;
     setTwoWeeks(startDate);
+    setFestivalTileList();
 }
 
 function alertIsNotThreeMonths() {
@@ -62,6 +140,7 @@ function alertIsNotThreeMonths() {
 function initTile(tile) {
     tile.className = 'mainDayList';
     tile.querySelector('.month')?.remove();
+    tile.querySelector('button').onclick = null;
 }
 
 function setMonthTag(date, tile) {
@@ -97,7 +176,7 @@ function setDayTile(date, tile) {
         tile.classList.add('on');
     }
 
-    button.addEventListener('click', (_) => onSelectDay(date));
+    button.onclick = (_) => onSelectDay(date);
 }
 
 function onSelectDay(date) {
@@ -109,7 +188,9 @@ function onSelectDay(date) {
         if (el.classList.contains(formatDateToYYYYMMDD(date))) {
             el.classList.add('on');
         }
-    })
+    });
+    selectedDate = date;
+    setFestivalTileList();
 }
 
 function formatDateToYYYYMMDD(date) {
@@ -117,6 +198,13 @@ function formatDateToYYYYMMDD(date) {
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
     return `${year}${month}${day}`;
+}
+
+function formatDateToDotYMD(date) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1);
+    const day = String(date.getDate());
+    return `${year}. ${month}. ${day}.`;
 }
 
 function isSameDay(a, b) {
@@ -136,4 +224,110 @@ function isWithinThreeMonths(date1, date2) {
   limit.setMonth(limit.getMonth() + 3);
 
   return later <= limit;
+}
+
+// festival function ----------------------------------------------------------------
+
+function getFestivalsForSelectedDay() {
+    return festivals.filter((el) => el.startDate <= selectedDate && el.endDate >= selectedDate);
+}
+
+function setFestivalTileList() {
+    ftSwiperWrapper.replaceChildren();
+
+    getFestivalsForSelectedDay().forEach((ft) => {
+        const divSlide = createFestivalTile(ft);
+        ftSwiperWrapper.appendChild(divSlide);
+    });
+}
+
+function createFestivalTile(festival) {
+    const divSlide = document.createElement('div');
+    divSlide.className = 'swiper-slide';
+
+    const divInner = document.createElement('div');
+    divInner.className = 'inner';
+
+    const spanImg = createFtImg(festival);
+    const divInfo = createFtInfo(festival);
+
+    divInner.appendChild(spanImg);
+    divInner.appendChild(divInfo);
+
+    divSlide.appendChild(divInner);
+
+    return divSlide;
+}
+
+function createFtImg(festival) {
+    const spanImg = document.createElement('span');
+    spanImg.className = 'img';
+
+    const img = document.createElement('img');
+    img.src = festival.image;
+    img.alt = festival.title;
+
+    spanImg.appendChild(img);
+
+    return spanImg;
+}
+
+function createFtInfo(festival) {
+    const divInfo = document.createElement('div');
+    divInfo.className = 'info';
+
+    const h3Title = document.createElement('h3');
+    h3Title.innerHTML = festival.title;
+
+    const emAddr = document.createElement('em');
+    emAddr.innerHTML = festival.addr;
+
+    const divPeriodPlace = document.createElement('div');
+    divPeriodPlace.className = 'period_place';
+    const divPeriod = createFtInfoPeriod(festival);
+    const divPlace = createFtInfoPlace(festival);
+    divPeriodPlace.appendChild(divPeriod);
+    divPeriodPlace.appendChild(divPlace);
+
+    const divBtn = createFtInfoBtn();
+
+    divInfo.appendChild(h3Title);
+    divInfo.appendChild(emAddr);
+    divInfo.appendChild(divPeriodPlace);
+    divInfo.appendChild(divBtn);
+
+    return divInfo;
+}
+
+function createFtInfoPeriod(festival) {
+    const divPeriod = document.createElement('div');
+    divPeriod.className = 'period';
+
+    divPeriod.innerHTML = 
+    `<strong>기간</strong>
+    <span>${formatDateToDotYMD(festival.startDate)} ~ ${formatDateToDotYMD(festival.endDate)}</span>`
+
+    return divPeriod;
+}
+
+function createFtInfoPlace(festival) {
+    const divPlace = document.createElement('div');
+    divPlace.className = 'place';
+
+    divPlace.innerHTML = 
+    `<strong>장소</strong>
+    <span>${festival.detailAddr}</span>`
+
+    return divPlace;
+}
+
+function createFtInfoBtn() {
+    const divBtn = document.createElement('div');
+    divBtn.className = 'btn';
+
+    divBtn.innerHTML = 
+    `<a href="#">바로가기</a>
+    <a href="#">길찾기</a>`;
+
+    return divBtn;
 }
