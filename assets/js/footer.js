c716fe6da1e94aa3fb9b7a3ffe69ba370e5d1ac2
jhpark0888 <jhpark0888@naver.com>
2025-04-17 17:14:43 +0900

    [fix] - footer SNS이미지 변경 시 querySelector 오류 해결

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -5,10 +5,10 @@ fetch('footer.html')
  
     const footerSNSList = document.querySelectorAll('footer .footer_sns_list ul li a');
 
-    for (const i in footerSNSList) {
-        const a = footerSNSList[parseInt(i)];
+    for (let i = 0; i < footerSNSList.length; i++) {
+        const a = footerSNSList[i];
         const img = a.querySelector('img');
-        const imgIdx = parseInt(i) + 1;
+        const imgIdx = i + 1;
         a.addEventListener('mouseover', () => {
             img.src = `./assets/images/footer/ico_sns${imgIdx}_1.png`;
         });
